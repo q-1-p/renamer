84c6d5f0e77a7068e2042d0e5a7efa0dbc2f086c
q-1-p <github.pubilc@outlook.com>
2025-07-20 12:30:40 +0900

    chore: Setup biome

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
-	return (
-		<div>
-			<h1 className="text-red-500">Test</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="text-red-500">Test</h1>
+    </div>
+  );
 }
